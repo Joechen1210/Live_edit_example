38d25df9446daf44f4cc74aebd5459387189f963
Joechen1210 <joes9630003@gmail.com>
2016-01-06 16:13:43 +0800

    Update live_example.js

--- javascript/live_example.js
+++ javascript/live_example.js
@@ -65,10 +65,10 @@ videojs.plugin('firstPlugin', function() {
     player.el().appendChild(postdiv);
   }
 
-/*document.getElementById('right1').addEventListener("click", middledivevent1());
-document.getElementById("right2").addEventListener("click", middledivevent2());
+document.getElementById('right1').addEventListener("click", middledivevent1());
+/*document.getElementById("right2").addEventListener("click", middledivevent2());
 document.getElementById("right3").addEventListener("click", middledivevent3());
-document.getElementById("right4").addEventListener("click", middledivevent4());
+document.getElementById("right4").addEventListener("click", middledivevent4());*/
 
 function middledivevent1()
 {
