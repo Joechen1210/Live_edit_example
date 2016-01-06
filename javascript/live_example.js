e1099b4a7ddbff8d556611b73ca25ee4a93b540e
Joechen1210 <joes9630003@gmail.com>
2016-01-06 11:00:42 +0800

    Update live_example.js

--- javascript/live_example.js
+++ javascript/live_example.js
@@ -10,7 +10,7 @@ videojs.plugin('firstPlugin', function() {
     var postdiv = document.createElement('div');
     postdiv.className = 'vjs-overlay';
     postdiv.className += ' vjs-overlay-right1';
-    overlay.innerHTML = "<img src='https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/agenda-logo.png' max-width='200px' max-height='200px' width='100%' height='100%'></img>";
+    postdiv.innerHTML = "<img src='https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/agenda-logo.png' max-width='200px' max-height='200px' width='100%' height='100%'></img>";
     //postdiv.className += ' vjs-overlay-right' + i ;
     console.log("create"+postdiv.className);
     player.el().appendChild(postdiv);
