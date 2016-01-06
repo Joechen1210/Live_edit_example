2979881a346815be718e20e65213fbbfd619dd7a
Joechen1210 <joes9630003@gmail.com>
2016-01-06 16:56:05 +0800

    Update live_example.js

--- javascript/live_example.js
+++ javascript/live_example.js
@@ -67,9 +67,9 @@ videojs.plugin('firstPlugin', function() {
   }
 
 document.getElementById('right1').addEventListener("click", middledivevent1);
-document.getElementById("right2").addEventListener("click", middledivevent2());
-/*document.getElementById("right3").addEventListener("click", middledivevent3());
-document.getElementById("right4").addEventListener("click", middledivevent4());*/
+document.getElementById("right2").addEventListener("click", middledivevent2;
+/*document.getElementById("right3").addEventListener("click", middledivevent3);
+document.getElementById("right4").addEventListener("click", middledivevent4);*/
 
 function middledivevent1()
 {
