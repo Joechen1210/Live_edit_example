4b4002e96c6a70214c976431c3ac077705763331
Joechen1210 <joes9630003@gmail.com>
2016-01-06 10:18:12 +0800

    Update live_example.js

--- javascript/live_example.js
+++ javascript/live_example.js
@@ -1,7 +1,16 @@
 videojs.plugin('firstPlugin', function() {
   var player = this,
-    overlay = document.createElement('p');
+  overlay = document.createElement('p');
   overlay.className = 'vjs-overlay';
   overlay.innerHTML = "First Plugin Working!";
   player.el().appendChild(overlay);
+  
+  for(i=1;i<5;i++)
+  {
+    var postdiv = document.createElement('div');
+    postdiv.className = 'vjs-overlay';
+    postdiv.className += 'vjs-overlay-right' + i ;
+    console.log("create"+postdiv.className);
+    player.el().appendChild(postdiv);
+  }
 });
