cf8b4329d42536aec21a42344743688ec1131a25
Joechen1210 <joes9630003@gmail.com>
2016-01-06 11:19:20 +0800

    Update live_example.js
    Edit Image url

--- javascript/live_example.js
+++ javascript/live_example.js
@@ -10,7 +10,7 @@ videojs.plugin('firstPlugin', function() {
     var postdiv = document.createElement('div');
     postdiv.className = 'vjs-overlay';
     postdiv.className += ' vjs-overlay-right1';
-    postdiv.innerHTML = "<img src='hhttps://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/date_and_time_clock-512.png' max-width='200px' max-height='200px' width='100%' height='100%'></img>";
+    postdiv.innerHTML = "<img src='https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/date_and_time_clock-512.png' max-width='200px' max-height='200px' width='100%' height='100%'></img>";
     //postdiv.className += ' vjs-overlay-right' + i ;
     console.log("create"+postdiv.className);
     player.el().appendChild(postdiv);
