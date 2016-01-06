9ff575a9b702ea028a5e6f82a4d39e61fecd94ac
Joechen1210 <joes9630003@gmail.com>
2016-01-06 11:27:26 +0800

    Update live_example.js

--- javascript/live_example.js
+++ javascript/live_example.js
@@ -5,14 +5,14 @@ videojs.plugin('firstPlugin', function() {
   overlay.innerHTML = "First Plugin Working!";
   player.el().appendChild(overlay);
   
-  //for(i=1;i<5;i++)
-  //{
+  for(i=0;i<2;i++)
+  {
     var postdiv = document.createElement('div');
     postdiv.className = 'vjs-overlay';
-    postdiv.className += ' vjs-overlay-right1';
+    postdiv.className += ' vjs-overlay-right'+ i;
     postdiv.innerHTML = "<img src='https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/date_and_time_clock-512.png' max-width='200px' max-height='200px' width='100%' height='100%'></img>";
     //postdiv.className += ' vjs-overlay-right' + i ;
     console.log("create"+postdiv.className);
     player.el().appendChild(postdiv);
-  //}
+  }
 });
