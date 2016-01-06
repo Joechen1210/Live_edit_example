a39f987642081e166cd4cad119746730104528cf
Joechen1210 <joes9630003@gmail.com>
2016-01-06 13:59:17 +0800

    Update live_example.js

--- javascript/live_example.js
+++ javascript/live_example.js
@@ -4,13 +4,14 @@ videojs.plugin('firstPlugin', function() {
   weburl = [
             "https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/date_and_time_clock-512.png", 
             "https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/FacebookIcon1.png",
-            "https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/Twitter_Icon2.png"
+            "https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/Twitter_Icon2.png",
+            "https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/questionnaire_icon4.png"
             ];
   overlay.className = 'vjs-overlay';
   overlay.innerHTML = "First Plugin Working!";
   player.el().appendChild(overlay);
   
-  for(i=1;i<=3;i++)
+  for(i=1;i<=4;i++)
   {
     var postdiv = document.createElement('div');
     postdiv.className = 'vjs-overlay';
