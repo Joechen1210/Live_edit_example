75798038d5d7a61acab36422b59e9e9d55ea7cb8
Joechen1210 <joes9630003@gmail.com>
2016-01-06 14:29:37 +0800

    Update live_example.js

--- javascript/live_example.js
+++ javascript/live_example.js
@@ -6,7 +6,13 @@ videojs.plugin('firstPlugin', function() {
             "https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/FacebookIcon1.png",
             "https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/Twitter_Icon2.png",
             "https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Image/catering-button-questionnaire.png"
-            ];
+            ],
+  titletext = [
+               "Agenda",
+               "Facebook",
+               "Twitter",
+               "Questionnaire"
+              ];
   overlay.className = 'vjs-overlay';
   overlay.innerHTML = "First Plugin Working!";
   player.el().appendChild(overlay);
@@ -16,7 +22,7 @@ videojs.plugin('firstPlugin', function() {
     var postdiv = document.createElement('div');
     postdiv.className = 'vjs-overlay';
     postdiv.className += ' vjs-overlay-right'+ i;
-    postdiv.innerHTML = "<img src='" + weburl[i-1] + "' max-width='200px' max-height='200px' width='100%' height='100%'></img>";
+    postdiv.innerHTML = "<img title='" + titletext[i-1]  + "' src='" + weburl[i-1] + "' max-width='200px' max-height='200px' width='100%' height='100%'></img>";
     //postdiv.className += ' vjs-overlay-right' + i ;
     console.log("create"+postdiv.className);
     player.el().appendChild(postdiv);
