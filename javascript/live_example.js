0ce6fad96720c7f5cb25c257524b3baa51222360
Joechen1210 <joes9630003@gmail.com>
2016-01-06 16:52:32 +0800

    Update live_example.js

--- javascript/live_example.js
+++ javascript/live_example.js
@@ -73,15 +73,16 @@ document.getElementById("right4").addEventListener("click", middledivevent4());*
 
 function middledivevent1()
 {
-   /*var webcontent = "<iframe src='http://htmlpreview.github.io/?https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Example_html/example_html.html' width= '100%' height= '100%' marginheight= '0' marginwidth= '0' hspeace= '0' vspace= '0' frameborder= '0' scrolling= 'auto'></iframe>",
+   var webcontent = "<iframe src='" + weburl[0]  + "' width= '100%' height= '100%' marginheight= '0' marginwidth= '0' hspeace= '0' vspace= '0' frameborder= '0' scrolling= 'auto'></iframe>",
        changecontent = document.getElementById("divbody");
        console.log("display " + webcontent);
               if (typeof webcontent === 'string') {
-                   changecontent.innerHTML = "<iframe src='http://htmlpreview.github.io/?https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Example_html/example_html.html' width= '100%' height= '100%' marginheight= '0' marginwidth= '0' hspeace= '0' vspace= '0' frameborder= '0' scrolling= 'auto'></iframe>";
-               }
+                   //changecontent.innerHTML = "<iframe src='http://htmlpreview.github.io/?https://raw.githubusercontent.com/Joechen1210/Live_edit_example/master/Example_html/example_html.html' width= '100%' height= '100%' marginheight= '0' marginwidth= '0' hspeace= '0' vspace= '0' frameborder= '0' scrolling= 'auto'></iframe>";
+               changecontent.innerHTML = webcontent;
+              }
                 else {
                     changecontent.appendChild(webcontent);
-                     }*/
+                     }
    if (document.getElementById("divparent")) 
           {
              console.log("Middle Div is visiable");
